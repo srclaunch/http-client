f44b0dac59a512d1847fc8ba3c9ad4ca6e221678
Steven Bennett <steven@budgetbloom.com>
2022-04-25 12:23:48 -0700

    Update

---
+++ .srclaunch/config.js
@@ -0,0 +1,30 @@
+import {
+  BuildFormat,
+  BuildPlatform,
+  BuildTarget,
+  BuildTool,
+  ProjectType,
+  TestReporter,
+  TestTool,
+} from '@srclaunch/types';
+
+export default {
+  name: '@srclaunch/actions',
+  description: 'SrcLaunch Actions',
+  type: ProjectType.Library,
+  build: {
+    bundle: {
+      exclude: ['axios', 'chalk', 'nanoid'],
+    },
+    formats: [BuildFormat.ESM, BuildFormat.UMD],
+    platform: BuildPlatform.Node,
+    target: BuildTarget.ESNext,
+    tool: BuildTool.Vite,
+  },
+  test: {
+    coverage: {
+      reporters: [TestReporter.Lcov, TestReporter.JSONSummary],
+    },
+    verbose: true,
+  },
+};
--- src/tests/http-client.test.ts
+++ src/tests/http-client.test.ts
@@ -1,10 +1,5 @@
 import test from 'ava';
-// import { Logger } from '../index';
 
 test('Truthy', t => {
   t.pass();
 });
-// test('Can create new Logger instance', t => {
-//   const logger = new Logger();
-//   t.truthy(logger);
-// });
